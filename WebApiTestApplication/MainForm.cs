62d644ee137072253bb0917aef8ca42f681be56a
gkalchev93 <gkalchev93@gmail.com>
2018-03-17 17:12:34 +0200

    Web Api Test App basic implementation

---
+++ WebApiTestApplication/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebApiCommunication;
+
+namespace WebApiTestApplication
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Client httpClient = new Client(@"http://localhost:65366");
+            var item = httpClient.GetContact(1);
+        }
+    }
+}
