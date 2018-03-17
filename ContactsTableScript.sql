214e0456bcd0af4316ca51ed031aac3fbaf19c4a
gkalchev93 <gkalchev93@gmail.com>
2018-03-17 17:11:33 +0200

    Table Script

---
+++ ContactsTableScript.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Contacts] (
+    [Id]        INT            NOT NULL,
+    [Name]      NVARCHAR (255) NOT NULL,
+    [Egn]       NVARCHAR (10)  NOT NULL,
+    [Address]   NVARCHAR (255) NOT NULL,
+    [Telephone] NCHAR (32)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
