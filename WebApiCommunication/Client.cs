968b5bc514e95cf7950995d18f76a09307f1b895
gkalchev93 <gkalchev93@gmail.com>
2018-03-17 17:12:17 +0200

    Web Api Communication Library basic implementation

---
+++ WebApiCommunication/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebApiCommunication.DTO;
+
+namespace WebApiCommunication
+{
+    public class Client
+    {
+        private HttpClient httpClient;
+        public Client(string path)
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(path);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<Contact> GetContact(int id)
+        {
+            Contact tmpObj = null;
+
+            HttpResponseMessage response = await httpClient.GetAsync($"/api/contacts/1");
+            if(response.IsSuccessStatusCode)
+            {
+                var k = response.Content.ReadAsStringAsync();
+                tmpObj = await response.Content.ReadAsAsync<Contact>();
+            }
+
+            return tmpObj;
+        }
+    }
+}
---
+++ WebApiCommunication/DTO/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiCommunication.DTO
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Egn { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
+    }
+}
