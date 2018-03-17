290c21d6072c1611a2d602c04de4fcef7b6ac456
gkalchev93 <gkalchev93@gmail.com>
2018-03-17 21:21:17 +0200

    Updated Table Query

--- ContactsTableScript.sql
+++ ContactsTableScript.sql
@@ -1,8 +1,20 @@
-﻿CREATE TABLE [dbo].[Contacts] (
-    [Id]        INT            NOT NULL,
-    [Name]      NVARCHAR (255) NOT NULL,
-    [Egn]       NVARCHAR (10)  NOT NULL,
-    [Address]   NVARCHAR (255) NOT NULL,
-    [Telephone] NCHAR (32)     NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
+﻿USE [Contacts]
+GO
+
+/****** Object:  Table [dbo].[Contact]    Script Date: 3/17/2018 9:20:39 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Contact](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](255) NOT NULL,
+	[Egn] [numeric](10, 0) NOT NULL,
+	[Address] [varchar](255) NOT NULL,
+	[Telephone] [varchar](32) NOT NULL
+) ON [PRIMARY]
+GO
+
+
