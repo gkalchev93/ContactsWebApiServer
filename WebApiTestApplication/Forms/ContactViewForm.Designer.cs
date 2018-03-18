31f0a223b837023eae40dafe079c6836e4f9565d
gkalchev93 <gkalchev93@gmail.com>
2018-03-18 10:02:26 +0200

    ContactViewForm implementation

---
+++ WebApiTestApplication/Forms/ContactViewForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WebApiTestApplication.Forms
+{
+    partial class ContactViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbTelephone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbEgn = new System.Windows.Forms.MaskedTextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(29, 32);
+            this.tbName.MaxLength = 255;
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(227, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EGN:";
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(29, 121);
+            this.tbAddress.MaxLength = 255;
+            this.tbAddress.Multiline = true;
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(227, 43);
+            this.tbAddress.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address:";
+            // 
+            // tbTelephone
+            // 
+            this.tbTelephone.Location = new System.Drawing.Point(29, 188);
+            this.tbTelephone.MaxLength = 32;
+            this.tbTelephone.Name = "tbTelephone";
+            this.tbTelephone.Size = new System.Drawing.Size(227, 20);
+            this.tbTelephone.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Telephone:";
+            // 
+            // tbEgn
+            // 
+            this.tbEgn.Location = new System.Drawing.Point(29, 78);
+            this.tbEgn.Mask = "0000000000";
+            this.tbEgn.Name = "tbEgn";
+            this.tbEgn.Size = new System.Drawing.Size(227, 20);
+            this.tbEgn.TabIndex = 2;
+            this.tbEgn.ValidatingType = typeof(int);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(181, 225);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(100, 225);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // ContactViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 255);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.tbEgn);
+            this.Controls.Add(this.tbTelephone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ContactViewForm";
+            this.Text = "ContactViewForm";
+            this.Load += new System.EventHandler(this.ContactViewForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbTelephone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox tbEgn;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ WebApiTestApplication/Forms/ContactViewForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebApiCommunication.DTO;
+
+namespace WebApiTestApplication.Forms
+{
+    public partial class ContactViewForm : Form
+    {
+        public Contact ContactItem { get; set; }
+
+        public ContactViewForm(Contact item)
+        {
+            InitializeComponent();
+            ContactItem = item;
+            
+        }
+
+        private void ContactViewForm_Load(object sender, EventArgs e)
+        {
+            if (ContactItem != null)
+            {
+                tbName.Text = ContactItem.Name;
+                tbEgn.Text = ContactItem.Egn;
+                tbAddress.Text = ContactItem.Address;
+                tbTelephone.Text = ContactItem.Telephone;
+                btnOk.Text = "Save";
+                this.Text += $" - ContactId = {ContactItem.Id}";
+            }
+            else
+            {
+                btnOk.Text = "Add";
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(tbName.Text))
+            {
+                MessageBox.Show("Please fill the Name field.");
+            }
+            else if(string.IsNullOrEmpty(tbEgn.Text) || tbEgn.Text.Length != 10)
+            {
+                MessageBox.Show("Please fill the EGN field.");
+            }
+            else if(string.IsNullOrEmpty(tbAddress.Text))
+            {
+                MessageBox.Show("Please fill the Address field.");
+            }
+            else if(string.IsNullOrEmpty(tbTelephone.Text))
+            {
+                MessageBox.Show("Please fill the Telephone field.");
+            }
+            else
+            {
+                if(ContactItem != null)
+                    ContactItem = new Contact(ContactItem.Id, tbName.Text, tbEgn.Text, tbAddress.Text, tbTelephone.Text);
+                else
+                    ContactItem = new Contact(tbName.Text, tbEgn.Text, tbAddress.Text, tbTelephone.Text);
+
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+    }
+}
